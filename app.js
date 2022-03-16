cc1dccf72f8f829970ec610d7bd285e6ea58a3fd
Vishal Tiwari <gvishaltiwari@gmail.com>
2022-03-16 18:49:58 +0530

    removed cors

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const expres = require('express');
 const app = expres();
 const port = 3300;
-const cors = require('cors');
+//const cors = require('cors');
 const uri = process.env.MONGODB_URI;
 const cookieparse = require('cookie-parser');
 const db = require('./config/mongoose');
 app.use(expres.json());
 
-app.use(cors());
+//app.use(cors());
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
